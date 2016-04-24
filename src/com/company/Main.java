6e6272857ff138c7b4c0e74bb853be061027188c
vlad405 <vladkotvickiy@mail.ru>
2016-04-24 23:46:08 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int x = 2;
+            int y = 12;
+
+            //x = x * 3;
+            y = x + y;
+            x = y - x;
+            y = y - x;
+
+            System.out.println(x);
+            System.out.println(y);
+        }
+
+}
